efa9e6ab61491f864913290d5c8ea537fcc54472
Simon Isupov <is_se@mail.ru>
2024-04-02 23:25:57 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+int Accerman (int n)
+{
+    if (n==1)
+    {
+        return 1;
+    }
+    return n*Accerman(n-1); 
+}
+
+System.Console.WriteLine(Accerman(5));
+
